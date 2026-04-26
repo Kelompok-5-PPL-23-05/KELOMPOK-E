3292bcb711170c2791d4cc60a9589a01c75e0c02
salwafadiyah27 <salwafadiyah27@gmail.com>
2026-04-26 15:43:09 +0700

    PPLE-5 implementasi fitur pilih kelas untuk guru

---
+++ Template Laravel/database/seeders/KelasSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Kelas;
+
+class KelasSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $kelas = [
+            ['nama_kelas' => 'Paket A Kelas 1'],
+            ['nama_kelas' => 'Paket A Kelas 2'],
+            ['nama_kelas' => 'Paket B Kelas 1'],
+            ['nama_kelas' => 'Paket B Kelas 2'],
+            ['nama_kelas' => 'Paket C Kelas 1'],
+            ['nama_kelas' => 'Paket C Kelas 2'],
+        ];
+
+        foreach ($kelas as $k) {
+            Kelas::create($k);
+        }
+    }
+}
