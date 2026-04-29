767eb0e19ccf72ed99072a63ee5de16206a11b33
Imaderaditya05 <imaderaditya@student.telkomuniversity.ac.id>
2026-04-29 13:20:59 +0700

    fitur lembaga

--- Template Laravel/resources/views/admin/dashboard.blade.php
+++ Template Laravel/resources/views/admin/dashboard.blade.php
@@ -410,12 +410,12 @@
 
             <!-- Lembaga -->
             <div class="nav-section">
-                <div class="nav-section-title" onclick="this.classList.toggle('open'); document.getElementById('c-lembaga').classList.toggle('open');">
+                <div class="nav-section-title open" onclick="this.classList.toggle('open'); document.getElementById('c-lembaga').classList.toggle('open');">
                     <svg class="arrow" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5"><path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7"/></svg> Lembaga
                 </div>
-                <div class="nav-children" id="c-lembaga">
-                    <div class="nav-child-item">Profil Lembaga</div>
-                    <div class="nav-child-item">Data Lembaga</div>
+                <div class="nav-children open" id="c-lembaga">
+                    <a href="{{ route('admin.lembaga') }}" class="nav-child-item">Profil Lembaga</a>
+                    <a href="{{ route('admin.lembaga.edit') }}" class="nav-child-item">Edit Lembaga</a>
                 </div>
             </div>
 
